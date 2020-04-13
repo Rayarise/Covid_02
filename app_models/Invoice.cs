d9b2a17816c01a60851660a629f5c94931067b69
Rayarise <rayarise23@gmail.com>
2020-04-13 14:21:05 -0400

    3.2

---
+++ app_models/Invoice.cs
@@ -0,0 +1,32 @@
+﻿using app_models;
+using System;
+
+namespace BillingManagement.Models
+{
+    class Invoice
+    {
+
+        public int InvoiceId { get; set; }
+
+        DateTime CreationDateTime;
+
+        Customer customer { get; set; }
+
+        public Double SubTotal { get; set; }
+        public Double Fedtax { get; set; }
+        public Double ProvTax { get; set; }
+        public Double Total { get; set; }
+
+        public Invoice() {
+
+            CreationDateTime = DateTime.Now;
+
+        }
+
+        public Invoice(Customer c)
+        {
+            customer = c;
+            CreationDateTime = DateTime.Now;
+        }
+    }
+}
